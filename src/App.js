01674ec2debd472137acdfb05f90545fa17bd5d0
aditya6521 <adityasinghr15@gmail.com>
2021-05-16 01:56:37 +0530

    feat search function

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 import Movie from './comp/movie';
 
 const APIURL="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const SEARCHAPI="https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
 
 function App() {
   const [Movies, setMovies] = useState([]);
+  const [searchTerm, setsearchTerm]=useState("");
 
   useEffect(() => {
     fetch(APIURL)
@@ -18,9 +20,39 @@ function App() {
     })
   }, [])
 
+  const submitHandler=(e)=>{
+    e.preventDefault();
+
+    if(searchTerm){
+      fetch(SEARCHAPI + searchTerm)
+    .then((res)=>res.json())
+    .then((data)=>{
+      console.log(data);
+      setMovies(data.results);
+    });
+
+    setsearchTerm("");
+    }
+  }
+
+  const onchangeHandle=(e)=>{
+    setsearchTerm(e.target.value);
+  }
+
   return (
     <div>
-       <header><h1>CineLab</h1></header>
+       <header>
+         <h1>CineLab</h1>
+         </header>
+         <div className="search-bar">
+           <form onSubmit={submitHandler}>
+           <input className="search" 
+           type="search" 
+           placeholder="Search Movie.."
+           value={searchTerm}
+           onChange={onchangeHandle}/>
+           </form>
+         </div>
       <div className="AppB">
     {Movies.map(mov=>(
       <Movie key={mov.id} {...mov}/>
